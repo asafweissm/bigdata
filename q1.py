07163f163b457d2208ef9283c78585a6798f5909
Asaf Weissman <asafweissm@gmail.com>
2013-11-08 16:40:59 +0200

    q1 d

---
+++ q1.py
@@ -0,0 +1,17 @@
+def pnlprod(to):
+    prod = 1
+    for i in range(1, to+1):
+        prod *= ((1-2**(-i))**(-1))
+    return prod
+    
+def p(n, l):
+    s = 0
+    for j in range(0, l): # 0 ... l-1
+        s += (-1)**j * 2**(-0.5*j*(j-1)) * (1 - 2**(j-l))**n * pnlprod(j) * pnlprod(l - 1 - j)
+    return s
+
+if __name__ == '__main__':
+    for i in range(20):
+        print(p(i, 1))
+    for i in range(20):
+        print(p(i, 2))
